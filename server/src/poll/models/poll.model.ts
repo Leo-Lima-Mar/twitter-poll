d473a41be7eb2a32f5dd9648da81db3b2f1b67b4
Leonardo Marinho <leonardomarinho_10@hotmail.com>
2018-07-19 10:36:36 -0300

    Now hashtags are case insensitive

--- server/src/poll/models/poll.model.ts
+++ server/src/poll/models/poll.model.ts
@@ -11,6 +11,7 @@ export class Poll {
         }
 
     private normalizeHashtag(): void {
+        this.hashtag = this.hashtag.toLowerCase();
         this.hashtag = this.hashtag.replace(/\s/g, '');
         this.hashtag = this.hashtag.replace(/#/g, '');
         this.hashtag = `#${this.hashtag}`;
@@ -18,6 +19,7 @@ export class Poll {
 
     private normalizeOptions(): void {
         for (const option of this.options) {
+            option.hashtag = option.hashtag.toLowerCase();
             option.hashtag = option.hashtag.replace(/\s/g, '');
             option.hashtag = option.hashtag.replace(/#/g, '');
             option.hashtag = `#${option.hashtag}`;
